6c0f06028580201829ac0d67a33c6f0400f3bdde
Emil Guseynov <emilgemilgemilg@gmail.com>
2023-01-20 20:58:50 +0400

    model created

---
+++ CoinPrice/Model/Currency.swift
@@ -0,0 +1,13 @@
+//
+//  Currency.swift
+//  CoinPrice
+//
+//  Created by Emil Guseynov on 19.01.2023.
+//
+
+import Foundation
+
+struct Currency: Decodable {
+    let assetId: String
+    let url: String
+}
