49cc3177378d1c49b6176549a54aa471b5e9413e
Emil Guseynov <emilgemilgemilg@gmail.com>
2023-01-23 21:07:14 +0400

    created viewController with exchange rates

---
+++ CoinPrice/ExchangeViewController.swift
@@ -0,0 +1,16 @@
+//
+//  ExchangeViewController.swift
+//  CoinPrice
+//
+//  Created by Emil Guseynov on 20.01.2023.
+//
+
+import UIKit
+
+class ExchangeViewController: UIViewController, Coordinating {
+    var coordinator: Coordinator?
+    
+    override func viewDidLoad() {
+        view.backgroundColor = .brown
+    }
+}
